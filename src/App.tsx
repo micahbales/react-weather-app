ba4d135b08eecc4fbe12a3537a809debf49a6953
Micah Bales <micahbales@gmail.com>
2018-07-01 23:33:47 -0400

    Display Icon and High/Low Temps
    It might be better to load our data in the index and then pass it down into the component, but this works alright for now.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import './App.css';
 import WeatherSquare from './WeatherSquare'
 
-class App extends React.Component <{}, {data: any}> {
+class App extends React.Component <{}, {weatherData: any}> {
   public componentDidMount() {
 
     navigator.geolocation.getCurrentPosition((location) => {
@@ -13,22 +13,27 @@ class App extends React.Component <{}, {data: any}> {
 
       // Get weather data
     return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${userLat}&lon=${userLng}&APPID=${process.env.REACT_APP_OPEN_WEATHER_MAP}`)
-    .then(res => this.setState({data: res}));
+    .then(data => this.setState({weatherData: data}));
     });
   }
   
   public render() {
-    return (
-      <div className="app">
-        <WeatherSquare 
-          day={moment().format('dddd')}
-          icon="icon.ico"
-          high="77"
-          low="53"
-        />
-        <p>{'data: ' + JSON.stringify(this.state)}</p>
-      </div>
-    );
+    if (this.state) {
+      return (
+        <div className="app">
+          <WeatherSquare 
+            day={moment().format('dddd')}
+            icon={`${this.state.weatherData.data.weather[0].icon}.png`}
+            high={this.state.weatherData.data.main.temp_max}
+            low={this.state.weatherData.data.main.temp_min}
+          />
+          {/* <p>{JSON.stringify(this.state)}</p> */}
+        </div>
+      );
+    } else {
+      return <div className="app">Loading...</div>
+    }
+    
   }
 }
 
