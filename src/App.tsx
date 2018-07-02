4ca3b1d36bea16a381aae7789f3e86cf1a1605b6
Micah Bales <micahbales@gmail.com>
2018-07-01 22:04:30 -0400

    Load Weather Data for Fixed Coordinates
    We're using Axios here because it has a nice, promise-based API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import axios from 'axios';
 import * as React from 'react';
 import './App.css';
 import WeatherSquare from './WeatherSquare'
 
 class App extends React.Component {
+  public componentDidMount() {
+    // Get weather data
+    return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=38.8900660&lon=-76.9303910&APPID=${process.env.REACT_APP_OPEN_WEATHER_MAP}`)
+      .then(res => this.setState({data: res}));
+  }
+  
   public render() {
     return (
       <div className="app">
@@ -12,6 +19,7 @@ class App extends React.Component {
           high="77"
           low="53"
         />
+        <p>{'data: ' + JSON.stringify(this.state)}</p>
       </div>
     );
   }
