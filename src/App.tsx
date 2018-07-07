6cd5750f32c05269efcfe2271f48c81561b1a804
Micah Bales <micahbales@gmail.com>
2018-07-07 10:57:24 -0400

    Display Temp Max and Min for 5 Day Forecast
    This isn't exactly what we want. Open Weather Map doesn't seem to have daily high/lows for forecast weather. Looks like we need to switch to another API.

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,23 @@ import './App.css';
 import WeatherSquare from './WeatherSquare'
 const units = 'imperial';
 
-class App extends React.Component <{}, {weatherData: any}> {
+class App extends React.Component <{}, {currentData: any, forecastData: any}> {
   public componentDidMount() {
     navigator.geolocation.getCurrentPosition((location) => {
       const userLat = location.coords.latitude;
       const userLng = location.coords.longitude;
       
-      // Get weather data
-    return axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${userLat}&lon=${userLng}&units=${units}&APPID=${process.env.REACT_APP_OPEN_WEATHER_MAP}`)
-    .then(data => this.setState({weatherData: data}));
+    // Get weather data (seems like we have to do two calls to get both current and forecast data?)
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${userLat}&lon=${userLng}&units=${units}&APPID=${process.env.REACT_APP_OPEN_WEATHER_MAP}`)
+      .then((currentData) => {
+        axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${userLat}&lon=${userLng}&units=${units}&APPID=${process.env.REACT_APP_OPEN_WEATHER_MAP}`)
+          .then((forecastData) => {
+          this.setState({
+            currentData, 
+            forecastData
+          });
+        });
+      });
     });
   }
   
@@ -24,9 +32,9 @@ class App extends React.Component <{}, {weatherData: any}> {
           <div className="weather-now">
             <WeatherSquare 
               day={'Right Now'}
-              icon={`${this.state.weatherData.data.weather[0].icon}.png`}
-              high={this.state.weatherData.data.main.temp_max}
-              low={this.state.weatherData.data.main.temp_min}
+              icon={`${this.state.currentData.data.weather[0].icon}.png`}
+              high={this.state.currentData.data.main.temp_max}
+              low={this.state.currentData.data.main.temp_min}
               units={units}
             />
           </div>
@@ -34,22 +42,24 @@ class App extends React.Component <{}, {weatherData: any}> {
             <h2>Five-Day Forecast</h2>
             <div className="five-day-forecast">
               {
-                [1,2,3,4,5].map((num) => {
-                return (
-                  <WeatherSquare 
-                    day={moment().format('dddd')}
-                    icon={`${this.state.weatherData.data.weather[0].icon}.png`}
-                    high={this.state.weatherData.data.main.temp_max}
-                    low={this.state.weatherData.data.main.temp_min}
-                    units={units}
-                    key={num}
-                  />)
+                [3,11,19,27,35].map((num) => {
+                  const dt = this.state.forecastData.data.list[num].dt * 1000;
+                  return (
+                    <WeatherSquare 
+                      day={moment(dt).format('dddd')}
+                      icon={`${this.state.forecastData.data.list[num].weather[0].icon}.png`}
+                      high={this.state.forecastData.data.list[num].main.temp_max}
+                      low={this.state.forecastData.data.list[num].main.temp_min}
+                      units={units}
+                      key={num}
+                    />
+                  )
                 })
               }
             </div>
           </div>
           
-          {/* <p>{JSON.stringify(this.state)}</p> */}
+          <p>{JSON.stringify(this.state.forecastData.data.temp)}</p>
         </div>
       );
     } else {
